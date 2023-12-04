3e0e38dddec74a402a32107564a1a97413dbc9a4
rohitnayal12 <rohitnayalkld@gmail.com>
2023-12-05 00:34:35 +0530

    middle_of_the_task

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const head=document.createElement("h1")
+let value=true
+const button=document.getElementsByClassName("button")
+if(value){
+    button.innertext="show more"
+}
+else{
+    button.innertext="show less"
+}
 head.innerHTML ="hello world"
 
 console.log(head)
