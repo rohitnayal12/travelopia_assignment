5fc3b29ca46f04d86c7ec2a6871b942804e15c5b
rohitnayal12 <rohitnayalkld@gmail.com>
2023-12-05 15:14:02 +0530

    responsiveness

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const head=document.createElement("h1")
-let value=true
-const button=document.getElementsByClassName("button")
-if(value){
-    button.innertext="show more"
-}
-else{
-    button.innertext="show less"
-}
-head.innerHTML ="hello world"
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const additionalContent = document.getElementById('additionalContent');
+  
+    toggleButton.addEventListener('click', function () {
+        if (additionalContent.classList.contains('hidden')) {
+            additionalContent.classList.remove('hidden');
+            toggleButton.textContent = 'Show Less';
+        } else {
+            additionalContent.classList.add('hidden');
+            toggleButton.textContent = 'Show More';
+        }
+    });
+});
 
-console.log(head)
